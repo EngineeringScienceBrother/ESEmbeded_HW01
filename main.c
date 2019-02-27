e0428cdb2f00b6f9aa6f71912a222ebdd184cd80
EngineeringScienceBrother <jajaj1215@gmail.com>
2019-02-27 15:58:21 +0800

    what you did

--- main.c
+++ main.c
@@ -5,19 +5,33 @@ int plus(int a, int b) { return a + b; }
 int minus(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
+int(*p[4])(int x, int y);
+
 int main()
 {
-	int a, c;
-	char b;
-	printf("key a Function \nEX: 1 + 1\n");
-	scanf("%i %c %i", &a, &b, &c);
-	//IF ELSE IF
-	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
-	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
-	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
-	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+	int a, c, result;
+	int b;
+	p[0] = plus;
+	p[1] = minus;
+	p[2] = multiply;
+	p[3] = divided;
+		
+	printf("Please Enter Two Numbers: \n");
+	scanf("%i %i", &a, &c);
+	
+	printf("0: Plus, 1: Minus, 2: Multiply, 3: Divided\n");
+	
+	do{	
+		printf("Please Enter the operation you want: ");
+		scanf("%d", &b);
+		
+	}
+
+	while(b<0 || b>3);
+	
+	result = (*p[b])(a, c);
+
+	printf("Result is = %d", result);
+
 }
+
